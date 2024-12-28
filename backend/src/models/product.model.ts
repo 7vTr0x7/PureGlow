e0ba464c2457e4b77a4df4d0dcf86ed09372ef58
7vTr0x7 <7vTrox7@gmail.com>
2024-12-29 00:38:20 +0530

    feat: product model

---
+++ backend/src/models/product.model.ts
@@ -0,0 +1,80 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface SkinHealthMetrics {
+  acneSuitability: number;
+  hydrationSuitability: number;
+  darkSpotsSuitability: number;
+  skinType: Array<"Normal" | "Oily" | "Dry" | "All Skin Type">;
+}
+
+interface IProduct extends Document {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  stock: number;
+  ratings: {
+    average: number;
+  };
+  skinHealthMetrics: SkinHealthMetrics;
+}
+
+const productSchema: Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  ratings: {
+    average: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+  },
+  skinHealthMetrics: {
+    acneSuitability: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    hydrationSuitability: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    darkSpotsSuitability: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    skinType: {
+      type: [String],
+      enum: ["Normal", "Oily", "Dry", "All Skin Type"],
+    },
+  },
+});
+
+const Product = mongoose.model<IProduct>("Product", productSchema);
+
+export default Product;
+export { IProduct };
