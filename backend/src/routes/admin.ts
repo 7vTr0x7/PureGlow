4825a6040e89fdb5164e29af97fe304184dc81c6
7vTr0x7 <7vTrox7@gmail.com>
2024-12-30 08:03:44 +0530

    feat:  updateProduct & deleteProduct

--- backend/src/routes/admin.ts
+++ backend/src/routes/admin.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { addProduct } from "../controllers/admin.js"
+import { addProduct, deleteProduct, updateProduct } from "../controllers/admin.js"
 
 const router:  Router = Router()
 
 router.get("/add/product",addProduct)
+router.put("/update/product/:id",updateProduct)
+router.delete("/delete/product/:id",deleteProduct)
 
 
 export default router
