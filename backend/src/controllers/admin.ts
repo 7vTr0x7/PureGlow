dce49311a0f5de89eda75c22cb2a885d561cb774
7vTr0x7 <7vTrox7@gmail.com>
2024-12-30 07:57:53 +0530

    feat: admin routes & controller

--- backend/src/controllers/admin.ts
+++ backend/src/controllers/admin.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import Product, { IProduct } from "../models/product.model.js"
 
+
+
 export const addProduct = async(req:Request,res:Response) => {
 try {
    const product = await Product.create()
@@ -14,5 +16,38 @@ try {
    }
 } catch (error) {
      res.status(500).json({success:false,message:"Failed to get product"})
+}
+}
+
+
+export const updateProduct = async(req:Request,res:Response) => {
+try {
+   const product = await Product.findByIdAndUpdate(req.params.id,req.body,{new:true})
+   if(product) {
+   res.json({
+    success:true,
+    product
+   })
+   }else{
+    res.status(404).json({success:false,message:"Product not found"})
+   }
+} catch (error) {
+     res.status(500).json({success:false,message:"Failed to update product"})
+}
+}
+
+export const deleteProduct = async(req:Request,res:Response) => {
+try {
+   const product = await Product.findByIdAndDelete(req.params.id)
+   if(product) {
+   res.json({
+    success:true,
+    product
+   })
+   }else{
+    res.status(404).json({success:false,message:"Product not found"})
+   }
+} catch (error) {
+     res.status(500).json({success:false,message:"Failed to delete product"})
 }
 }
--- backend/src/models/product.model.ts
+++ backend/src/models/product.model.ts
@@ -1,10 +1,17 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+enum SkinType {
+  Normal = "Normal",
+  Oily = "Oily",
+  Dry = "Dry",
+  All = "All Skin Type",
+}
+
 interface SkinHealthMetrics {
   acneSuitability: number;
   hydrationSuitability: number;
   darkSpotsSuitability: number;
-  skinType: Array<"Normal" | "Oily" | "Dry" | "All Skin Type">;
+  skinType: Array<SkinType>;
 }
 
 interface IProduct extends Document {
@@ -18,63 +25,73 @@ interface IProduct extends Document {
     average: number;
   };
   skinHealthMetrics: SkinHealthMetrics;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-const productSchema: Schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  stock: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  ratings: {
-    average: {
+const productSchema: Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
       type: Number,
+      required: true,
       min: 0,
-      max: 5,
-      default: 0,
+    },
+    stock: {
+      type: Number,
+      required: true
+      min: 0,
+    },
+    ratings: {
+      average: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0,
+      },
+    },
+    skinHealthMetrics: {
+      acneSuitability: {
+        type: Number,
+        min: 0,
+        max: 5,
+      },
+      hydrationSuitability: {
+        type: Number,
+        min: 0,
+        max: 100,
+      },
+      darkSpotsSuitability: {
+        type: Number,
+        min: 0,
+        max:5,
+      },
+      skinType: {
+        type: [String],
+        enum: Object.values(SkinType),
+      },
     },
   },
-  skinHealthMetrics: {
-    acneSuitability: {
-      type: Number,
-      min: 0,
-      max: 5,
-    },
-    hydrationSuitability: {
-      type: Number,
-      min: 0,
-      max: 5,
-    },
-    darkSpotsSuitability: {
-      type: Number,
-      min: 0,
-      max: 5,
-    },
-    skinType: {
-      type: [String],
-      enum: ["Normal", "Oily", "Dry", "All Skin Type"],
-    },
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Product = mongoose.model<IProduct>("Product", productSchema);
 
 export default Product;
-export { IProduct };
+export { IProduct, SkinType };
