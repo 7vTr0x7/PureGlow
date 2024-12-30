58dae8d2cabf950ed258bb63d2225f125f6e07ca
7vTr0x7 <7vTrox7@gmail.com>
2024-12-30 22:48:13 +0530

    fix

--- frontend/src/components/UserSkinScore.tsx
+++ frontend/src/components/UserSkinScore.tsx
@@ -9,7 +9,7 @@ const UserSkinScore: React.FC = () => {
     hydrationLevel: 0,
   });
 
-  const apiUrl = import.meta.env.VITE_API_URL;
+  const apiUrl = import.meta.env.VITE_API_URL
 
   const fetchUser = async (): Promise<void> => {
     try {
